b8240c84c7ad5f86750cf0d34712519510a21179
Guillermo Rodas <garethderioth@gmail.com>
2017-07-17 18:18:42 -0500

    Carousel modal styles

--- lessons/css/flexbox/carousel.js
+++ lessons/css/flexbox/carousel.js
@@ -1,3 +1,53 @@
+// Declaraciones de modulos
+
+// Modulo Carousel
+var carousel = (function() {
+  var carouselGridSelector = '.carousel-grid .carousel-item';
+
+  var showCarousel = function () {
+    // Mostrar el carousel
+    // toggleBody
+  }
+
+  var hideCarousel = function() {
+    // toggleBody
+  }
+
+  var nextSlide = function() {
+
+  }
+
+  var prevSlide = function() {
+
+  }
+
+  var setCurrentSlide = function() {
+
+  }
+
+  var cloneCarouselItems = function() {
+
+  }
+
+  var toggleBodyOverflowHidden =  function() {
+
+  }
+
+  var init = function() {
+
+  };
+
+  return {
+    init: init
+  }
+}());
+
+// Entrada de la app
+document.addEventListener('DOMContentLoaded', function() {
+  carousel.init();
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var carouselGridSelector = '.carousel-grid .carousel-item';
   var gridItems = document.querySelectorAll(carouselGridSelector);
