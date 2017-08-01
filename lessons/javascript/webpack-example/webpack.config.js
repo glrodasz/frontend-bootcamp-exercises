cfccdb558f2adf434fcfe32a821d62cbc42ae3ef
Guillermo Rodas <garethderioth@gmail.com>
2017-08-01 17:31:57 -0500

    Read SASS file

--- lessons/javascript/webpack-example/webpack.config.js
+++ lessons/javascript/webpack-example/webpack.config.js
@@ -8,7 +8,21 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      }
     ]
   }
 };
